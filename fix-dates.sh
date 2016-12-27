e04a2d552fc3cef9c70eda390300f780903a742d
nemoisdead <kmoochooloo@yahoo.com>
2016-12-28 00:57:17 +0330

    Add date check script
    This script checks to see if posts have date metadata. If not, it will
    add correct date based on authoring date of the post.

---
+++ fix-dates.sh
@@ -0,0 +1,9 @@
+for post in _posts/*.markdown
+do
+    if ! grep -Fq 'date: ' "$post"
+    then
+        date=$(git log --diff-filter=A --pretty='%ad' --date=iso --follow -- "$post")
+        sed -i "/title:/a\
+            date: $date" "$post"
+    fi
+done
