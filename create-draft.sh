5fff5fc25547654849e2b299b44245b14260360f
nemoisdead <kmoochooloo@yahoo.com>
2017-02-03 21:15:20 +0330

    Update create scripts
    Add script to create drafts.
    Move template to separate directory.
    Update scripts to match new structure.

--- create-draft.sh
+++ create-draft.sh
@@ -3,7 +3,7 @@ then
     echo "Error: Post name is required"
     exit 1
 else
-    name="_posts/$(date -I)-$1.markdown"
+    name="_drafts/$1.markdown"
     cp _drafts/templates/post-template.markdown "$name"
     gedit "$name"
 fi
